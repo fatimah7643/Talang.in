c37a1f8139bd3e0b07589b360cc8823c8a9d4a0f
fatimah7643 <fatimah013@gmail.com>
2026-05-22 12:46:29 +0800

    fix: Server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Supabase Client berhasil diinisialisasi.`);
   console.log(`Server Talang.in aktif dan standby di http://localhost:${PORT}`);
 });
