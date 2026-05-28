836e59e3405becebcc7449d832e7fd5d80305639
fatimah7643 <fatimah013@gmail.com>
2026-05-28 12:30:55 +0800

    feat: menambahkan enpoint Delete Notifikasi ALL dan By id

--- Backend/src/routes/notificationRoutes.js
+++ Backend/src/routes/notificationRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getNotifications, markAsRead, markAllAsRead, deleteNotification, deleteAllNotification } from '../controllers/notificationController.js'
+import { getNotifications, markAsRead, markAllAsRead, deleteNotification, deleteAllNotifications } from '../controllers/notificationController.js'
 import { authenticate } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
